a57cc4cb2146e253807d80e61dbf31f1938ca6b4
mwlevin <mlevin@umn.edu>
2025-03-05 14:54:58 -0600

    initial work spatial branching

--- main_CNDP.py
+++ main_CNDP.py
@@ -27,19 +27,21 @@ ins = 'HF_CNDP_1'
 #net = 'Anaheim'
 #ins = 'A_CNDP_30_1'
 
+scale_dem = 3
 b_prop = 0.5
 scal_flow = {'SiouxFalls':1e-3,'EasternMassachusetts':1e-3,'BerlinMitteCenter':1e-3,'Anaheim':1e-3,'Barcelona':1e-3, 'Braess':1, 'HarkerFriesz':1}
-inflate_trips = {'SiouxFalls':1,'EasternMassachusetts':4,'BerlinMitteCenter':2,'Anaheim':4,'Barcelona':2, 'Braess':1, 'HarkerFriesz':0.25}
+inflate_trips = {'SiouxFalls':1,'EasternMassachusetts':4,'BerlinMitteCenter':2,'Anaheim':4,'Barcelona':2, 'Braess':1, 'HarkerFriesz':0.25 * scale_dem}
 print(net,ins)
 
 inflate_cost = 5
-scale_dem = 1
-print(scale_dem * inflate_trips[net], inflate_cost, scal_flow[net])
+
+print(inflate_trips[net], inflate_cost, scal_flow[net])
 
 network = Network.Network(net,ins,b_prop,1e-0,scal_flow[net],inflate_trips[net])
 print("TD", network.TD)
 
-test = OA_CNDP_CG_SB.OA_CNDP_CG_SB(network, inflate_cost)
+test = OA_CNDP_CG.OA_CNDP_CG(network, inflate_cost)
+#test = OA_CNDP_CG_SB.OA_CNDP_CG_SB(network, inflate_cost)
 #test = HY_CNDP.HY_CNDP(network)
 #test = CNDP_MILP.CNDP_MILP(network, 5, 5, 20, inflate_cost)
 
